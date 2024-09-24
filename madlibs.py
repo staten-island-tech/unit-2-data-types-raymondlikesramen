576734be858bdb99750b6186d6191693091da980
raymondlikesramen <raymondchen31310@gmail.com>
2024-09-24 09:20:33 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,9 @@
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
+#asking a user to input a sentence.
+x=input('He was _ to school. write a verb')
+print(f'He was {x} to school')
+y=input('He saw _ _ over his classmate. write a celebrity, write a verb')
+print(f'He saw {y} over his classmate.')
+z=input('He was doing a math problem. _ was the answer. write a number')
+print(f'He was doing a math problem. {z} was the answer.')
+t=input(f'He finally walked out the _ . write a noun.')
+print(f'He finally walked out the {t}')
