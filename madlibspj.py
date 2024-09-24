ecebc16ae1ee30bfea75fca902f65c1491cf569f
raymondlikesramen <raymondchen31310@gmail.com>
2024-09-24 08:40:34 -0400

    Create madlibspj.py

---
+++ madlibspj.py
@@ -0,0 +1,7 @@
+#Asking a user to input a sentence.
+x = input('He was _ to school. write a verb ')
+print(f'He was {x} to school')
+y = input('_ _ over a little kid. write a noun, write a verb ')
+print(f'{y} over a little kid')
+z = input('Why was 9 scared of 7? Because 7,_,9. write a number')
+print(f'Why was 9 scared of 7, because 7,{z},9')
