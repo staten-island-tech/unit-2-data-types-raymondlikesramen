011617190ccd934e9b72adcff5436e10ffd9251e
raymondlikesramen <raymondchen31310@gmail.com>
2024-09-26 08:37:52 -0400

    Update tipcaluculator.py

---
+++ tipcaluculator.py
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+=======
+
+bill=0
+tip=0
+tap=0
+
+bill=float(input("how much is your bill"))
+tip=float(input("How much is tip?"))
+tap=bill*tip
+
+print(tap)
+>>>>>>> ce332762e3cbd7632d8d66a6e715c548d864fc4c
