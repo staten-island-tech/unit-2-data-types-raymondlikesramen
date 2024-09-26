d74e2abf0f2493dac4ed258d65df66b345921d4b
raymondlikesramen <raymondchen31310@gmail.com>
2024-09-26 09:22:37 -0400

    Update tipcalculator2.py

--- tipcalculator2.py
+++ tipcalculator2.py
@@ -1,5 +1,12 @@
-number_input= int(input("What number?" ))
-if number_input % 2 == 0:
-    print("even")
+bill=float(input('How much was the bill?'))
+service_quality=(input('How was the service? Bad, Okay, Good, Great?'))
+if service_quality == "Bad":
+    print(float(bill))
+elif service_quality == "Okay":
+    print(float(bill*1.15))
+elif service_quality == "Good":
+    print(float(bill*1.20))
+elif service_quality == "Great":
+    print(float(bill*1.25))
 else:
-    print("odd")
+    print('invalid input')
