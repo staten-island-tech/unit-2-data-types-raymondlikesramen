5fdf73b56d76690396204ae7a0266e22fdc945ac
raymondlikesramen <raymondchen31310@gmail.com>
2024-09-30 09:21:36 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,7 +1,7 @@
-t=int(input('Input number 1'))
-y=int(input('Input number 2'))
-
-for x in range(2, t+1 and 2, y+1):
-    if t % x == 0 and y % x == 0:
-        print(x)
-
+num1= int(input('number'))
+num2=int(input('number'))
+gcf=1
+for i in range(1, min(num1, num2) + 1):
+    if num1 % i == 0 and num2 % i == 0:
+        gcf = i
+print (f'{gcf} is the gcf')
